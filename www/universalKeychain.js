1b4acdad08e729b5747c2e553f68f68763fe9d66
Matt Hamel <matt@blispay.com>
2016-05-05 09:17:57 -0400

    update

--- www/universalKeychain.js
+++ www/universalKeychain.js
@@ -2,6 +2,13 @@ var exec = require("cordova/exec"),
     channel = require("cordova/channel");
 
 
+
+channel.onCordovaReady.subscribe(function(){
+    exec(win, null, 'Plugin', 'method', []);
+    function win(message){
+        console.log(message);
+    }
+});
 var UniversalKeychain = function(){
 
 };
